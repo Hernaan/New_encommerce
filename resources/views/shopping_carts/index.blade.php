22ae1bd0608a35c4d02d050b29ca7ea76a0a9cd7
Hernaan <hernachaparro@gmail.com>
2018-01-18 09:49:48 -0300

    Vista del carrito

--- resources/views/shopping_carts/index.blade.php
+++ resources/views/shopping_carts/index.blade.php
@@ -1,4 +1,28 @@
 @extends("layouts.app")
 @section("content")
-<h1>{{ $total }}</h1><!--$total se llama desde el controlador -->
+<div class="">
+	<h1 class="text-center">Tu carrito de compras</h1>
+</div>
+<div class="container">
+	<table class="table table-bordered">
+		<thead>
+			<tr>
+				<td>Producto</td>
+				<td>Precio</td>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach($products as $product)
+				<tr>
+					<td>{{ $product->title }}</td>
+					<td>{{ $product->pricing }}</td>
+				</tr>
+			@endforeach
+				<tr>
+					<td>Total</td>
+					<td>{{ $total }}</td><!--$total se llama desde el controlador -->
+				</tr>
+		</tbody>
+	</table>
+</div>
 @endsection
